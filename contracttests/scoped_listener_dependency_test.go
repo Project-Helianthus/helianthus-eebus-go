002590f74f5c7161a3b251f593f0da5c00f89ac3
Răzvan Corneliu C.R. VILT <16434603+d3vi1@users.noreply.github.com.>
2026-07-26 06:25:47 +0300

    test: require SHIP concrete-endpoint runtime

--- contracttests/scoped_listener_dependency_test.go
+++ contracttests/scoped_listener_dependency_test.go
@@ -14,11 +14,11 @@ import (
 
 const (
 	scopedListenerShipModule         = "github.com/Project-Helianthus/helianthus-ship-go"
-	scopedListenerShipVersion        = "v0.6.1-helianthus.8"
+	scopedListenerShipVersion        = "v0.6.1-helianthus.9"
 	scopedListenerShipRepository     = "https://github.com/Project-Helianthus/helianthus-ship-go.git"
-	scopedListenerShipTagObject      = "d82eca9a87e5340cd83fcdf6b2a3049239c5533b"
-	scopedListenerShipCommit         = "c4c452b2fddb49030eef30215f20edd60f32735e"
-	scopedListenerShipTree           = "8cc8892a60fe945521112823ea93721ca7f808e4"
+	scopedListenerShipTagObject      = "707a075de4ee1148f38e6fe1e6e101f73747c1df"
+	scopedListenerShipCommit         = "cfb9e17c3e045b2a53bd39afef2d33cd38326bc5"
+	scopedListenerShipTree           = "4d300074668be337fbe39453f1183ef36ff547c6"
 	scopedListenerShipManifestSHA256 = "54f91f18ab094825f68db61cad0423b4fadf2720179a09d2168d7cd988a43097"
 	scopedListenerShipLicenseSHA256  = "c853996135802c50b3048937e48022bc00b41ff5f56a31cebe7d686bf91f87db"
 )
